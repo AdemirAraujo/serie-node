a151e754eeaf75a320feec8ef1a7622f20c12c1f
AdemirAraujo <ademir.t.araujo@gmail.com>
2018-10-28 19:58:19 -0300

    crud

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -14,18 +14,42 @@ router.use(authMiddleware);
 
 //Listar
 router.get('/', async (req, res) => {
-    res.send({ user: req.userId });
+    try{
+        const projects = await Project.find().populate('user');
+        return res.send({ projects });
+    } catch(err) {
+        return res.status(400).send( { error: 'Error loading Projects', err} );
+    }
 });
 
 //Lista 1 Projeto
 router.get('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+    try{
+        const project = await Project.findById(req.params.projectId).populate('user');
+        return res.send({ project });
+    } catch(err) {
+        return res.status(400).send( { error: 'Error loading Project', err} );
+    }
 });
 
 //Create Project
 router.post('/', async (req, res) => {
     try{
-        const project = await Project.create(req.body);
+        const {title, description, tasks } = req.body;
+        
+        //Cria o Projeto
+        const project = await Project.create({ title, description, user: req.userId });
+
+        //Cria os Tasks
+        await Promise.all(tasks.map( async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+            await projectTask.save();
+            project.task.push(projectTask);
+        }));
+
+        //Salva o Projeto
+        await project.save();
+
         return res.send({ project });
     } catch(err) {
         return res.status(400).send( { error: 'Error creating new Project', err} );
@@ -39,7 +63,12 @@ router.put('/:projectId', async (req, res) => {
 
 //Deletar
 router.delete('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+    try{
+        await Project.findByIdAndRemove(req.params.projectId);
+        return res.send();
+    } catch(err) {
+        return res.status(400).send( { error: 'Error deleting Project', err} );
+    }
 });
 
 module.exports = app => app.use('/projects', router);
